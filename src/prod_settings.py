5a3f1b29b8f9d313e08ffd1f20c707c3e1e21047
Aaron Madison <aaron.l.madison@gmail.com>
2012-02-07 20:31:44 -0600

    added production settings

---
+++ src/prod_settings.py
@@ -0,0 +1,16 @@
+from settings import *
+
+DEBUG = False
+
+
+# Email configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'noreply@assistantproiowa.com'
+EMAIL_HOST_PASSWORD =  '985fe059992944028741'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'noreply@assistantproiowa.com'
+
+MANAGERS = (
+    ('Linda Vernon', 'linda.vernon@assistantproiowa.com'),
+)
