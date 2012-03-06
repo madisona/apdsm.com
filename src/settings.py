f4f8569ce2b1de38db866283a9fd7c3312fe68c8
Aaron Madison <aaron.l.madison@gmail.com>
2012-03-06 09:06:11 -0600

    converted deployment to AppEngine Project

--- src/settings.py
+++ src/settings.py
@@ -1,27 +1,67 @@
 # Django settings for src project.
+#
+# https://developers.google.com/cloud-sql/docs/django
+#
 
+
+import os, sys
 from os.path import abspath, dirname, join
 
 PROJECT_DIR = abspath(dirname(__file__))
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+LIB_DIR = join(PROJECT_DIR, 'lib')
+if LIB_DIR not in sys.path:
+    sys.path.insert(0, LIB_DIR)
 
 ADMINS = (
     ("Aaron Madison", "aaron.l.madison@gmail.com"),
 )
-MANAGERS = ADMINS
-
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': join(PROJECT_DIR, 'local.db'),
-    }
+        'ENGINE': 'google.appengine.ext.django.backends.rdbms',
+        'INSTANCE': 'twomadisons.com:tc-assistantpro:assistantpro',
+        'NAME': 'assistantpro_web',
+        }
 }
 
+if (os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine') or
+    os.getenv('SETTINGS_MODE') == 'prod'):
+    # Running on production App Engine, so use a Google Cloud SQL database.
+    DEBUG = True
+    MANAGERS = (
+        ('Linda Vernon', 'linda.vernon@assistantproiowa.com'),
+    )
+    EMAIL_BACKEND = 'djangoappengine.mail.AsyncEmailBackend'
 
-TIME_ZONE = 'America/Chicago'
+    TEMPLATE_LOADERS = (
+        ('django.template.loaders.cached.Loader', (
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        )),
+    )
+
+else:
+    # local dev environment
+    DEBUG = True
+    MANAGERS = ADMINS
+    EMAIL_BACKEND = 'djangoappengine.mail.EmailBackend'
+
+    TEMPLATE_LOADERS = (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )
+
+
+# Specify a queue name for the async. email backend
+EMAIL_QUEUE_NAME = 'default'
+
+TEMPLATE_DEBUG = DEBUG
+
+# Email configuration
+DEFAULT_FROM_EMAIL = 'noreply@assistantproiowa.com'
+
+TIME_ZONE = 'UTC'
 LANGUAGE_CODE = 'en-us'
 
 USE_I18N = False
@@ -30,7 +70,7 @@ USE_L10N = False
 MEDIA_ROOT = ''
 MEDIA_URL = ''
 
-STATIC_ROOT = abspath(join(PROJECT_DIR, '..', 'web', 'static'))
+STATIC_ROOT = abspath(join(PROJECT_DIR, 'production_static'))
 STATIC_URL = '/static/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
@@ -45,12 +85,10 @@ STATICFILES_FINDERS = (
 
 SECRET_KEY = 'e!7j6@opp_d-2o%_o65i&kd0hw!%&($@9w3d$(a4l3opln-pz_'
 
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
+
 
 MIDDLEWARE_CLASSES = (
+    'google.appengine.ext.appstats.recording.AppStatsDjangoMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -59,7 +97,7 @@ MIDDLEWARE_CLASSES = (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
-ROOT_URLCONF = 'src.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     join(PROJECT_DIR, 'templates'),
@@ -76,15 +114,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 INSTALLED_APPS = (
     'debug_toolbar',
-#    'south',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+
     'djbootstrap',
     'web',
 )
